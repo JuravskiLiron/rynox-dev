e4b6f4e33a5a8f35524ec1cc738f1dcc58560e42
lironjuravski <juravskiliron@gmail.com>
2026-05-18 13:35:33 +0300

    a1hh1nim

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Ticker from "./components/Ticker";
 import Stats from "./components/Stats";
-import BeforeAfter from "./components/BeforeAfter";
-import Portfolio from "./components/Portfolio";
+//import BeforeAfter from "./components/BeforeAfter";
+//import Portfolio from "./components/Portfolio";
 import Process from "./components/Process";
 import Packages from "./components/Packages";
 import CustomPackage from "./components/CustomPackage"; // ← добавить
